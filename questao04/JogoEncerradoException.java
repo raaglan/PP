45a1a6c3aaa22d84fb4afe3c016955c496079e33
Celio Raglan <raglanpb@gmail.com>
2016-09-01 17:26:06 -0300

    Add files via upload

---
+++ questao04/JogoEncerradoException.java
@@ -0,0 +1,9 @@
+package questao04;
+
+public class JogoEncerradoException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	public JogoEncerradoException(){
+		super("Não é permitido tentar mais palpites, o jogo está encerrado");
+	}
+}
